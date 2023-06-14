970f33bbdff8517f1e9407ad3253e532cfe35a72
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-06-14 13:25:27 -0700

    Correction Task 11 , 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,35 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            result += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
-        else:
-            result += roman_dict[roman_string[i]]
-    return result
+  """Converts a Roman numeral to an integer.
 
+  Args:
+    roman_string: A string representing a Roman numeral.
+
+  Returns:
+    The integer value of the Roman numeral.
+
+  Raises:
+    ValueError: If the roman_string is not a string or None, or if it is not a valid Roman numeral.
+  """
+
+  if not isinstance(roman_string, str) or roman_string is None:
+    raise ValueError("roman_string must be a string")
+
+  roman_numerals = {
+    "M": 1000,
+    "D": 500,
+    "C": 100,
+    "L": 50,
+    "X": 10,
+    "V": 5,
+    "I": 1,
+  }
+
+  result = 0
+  for i in range(len(roman_string)):
+    if roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]:
+      result -= roman_numerals[roman_string[i]]
+    else:
+      result += roman_numerals[roman_string[i]]
+
+  return result
