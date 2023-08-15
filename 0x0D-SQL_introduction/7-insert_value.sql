0c26e55b040f571beaf8d9b1d0b88eb34aa11f92
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-08-15 14:49:03 -0700

    Bonus Task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- This script insert a new row in specified table
-INSERT INTO first_table(id, name) VALUES(89, "Best SCHOOL)";
+-- inserts a new row in table first_table
+-- in database hbtn_0c_0
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
